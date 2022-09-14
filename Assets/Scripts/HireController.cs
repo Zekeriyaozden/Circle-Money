12cfa5f8425eb429610802cb485a05d266d3e7c8
Zekeriyaozden <zekeriyaozden@icloud.com>
2022-09-14 19:39:38 +0300

    push

--- Assets/Scripts/HireController.cs
+++ Assets/Scripts/HireController.cs
@@ -10,12 +10,10 @@ public class HireController : MonoBehaviour
     private GameManager gm;
     private bool triggerFlag;
     private bool corFlag;
-    private bool hireDecrease;
     public GameObject image;
     private Image slide;
     void Start()
     {
-        hireDecrease = true;
         slide = image.GetComponent<Image>();
         corFlag = true;
         gm = GameObject.Find("GameManager").GetComponent<GameManager>();
@@ -38,33 +36,29 @@ public class HireController : MonoBehaviour
                 k += Time.deltaTime / 5f;
                 slide.fillAmount = k;
             }
-            slide.fillAmount = 0f;
-            if (notHiredList.Count > 0 && triggerFlag)
+
+            if (slide.fillAmount >= 1)
             {
-                notHiredList[0].gameObject.GetComponent<SplineFollowerDeneme>().isHiring = true;
-                GameObject temp = notHiredList[0];
-                notHiredList.Remove(temp);
-            }   
+                slide.fillAmount = 0;
+                if (notHiredList.Count > 0 && triggerFlag)
+                {
+                    notHiredList[0].gameObject.GetComponent<SplineFollowerDeneme>().isHiring = true;
+                    GameObject temp = notHiredList[0];
+                    notHiredList.Remove(temp);
+                }   
+            }
+            else
+            {
+                
+            }
         }
     }
 
-    private IEnumerator decreaseUI()
-    {
-        float k = slide.fillAmount;
-        while (k > 0 && hireDecrease)
-        {
-            yield return new WaitForEndOfFrame();
-            k -= Time.deltaTime / 3f;
-            slide.fillAmount = k;
-        }
-    }
-    
 
     private void OnTriggerStay(Collider other)
     {
         if (other.tag == "Player")
         {
-            hireDecrease = false;
             triggerFlag = true;
             if (corFlag)
             {
@@ -78,10 +72,8 @@ public class HireController : MonoBehaviour
     {
         if (other.tag == "Player")
         {
-            hireDecrease = true;
             triggerFlag = false;
             corFlag = true;
-            StartCoroutine(decreaseUI());
         }
     }
 }
